536a51c9c4f34b815b62b1c9fd8a2839bfca6352
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2018-11-03 15:36:10 -0400

    skip list working (complete ?) need file reader now

--- Trees/SkipList.cpp
+++ Trees/SkipList.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "SkipList.h"
 #include <string.h>
+#include <random>
+#include <iostream>
+#include <ctime>
 
 SkipList::SkipList()
 {
@@ -22,9 +25,12 @@ SkipList::SkipList()
 
 	nodeStorage[nodeStorage_index] = newNode; 
 	bottomLeftNode = &nodeStorage[nodeStorage_index]; 
+	topLeftNode = &nodeStorage[nodeStorage_index]; 
 	leftNodes[NumOflevels] = &nodeStorage[nodeStorage_index]; 
 	nodeStorage_index++; 
 
+	//seed random generator 
+	srand(std::time(0));
 }
 
 void SkipList::Insert(char in_key[])
@@ -32,46 +38,146 @@ void SkipList::Insert(char in_key[])
 	_insert(in_key); 
 }
 
+void SkipList::List()
+{
+	_traverse(); 
+}
+
 void SkipList::_insert(char in_key[])
 {
+	bool complete = false; 
+	bool makeNewNode = true; 
+	int searchRow = topLeftNode->level; 
+
+	skipNode *leaderNode = topLeftNode; 
+	skipNode *laggerNode = topLeftNode; 
+	skipNode *lastPathDown = topLeftNode; 
+
 	skipNode *currentNode = bottomLeftNode; 
-	while (strcmp(in_key, currentNode->key) > 0 )
+	skipNode *searchNode = topLeftNode; 
+
+
+	while (searchRow != 0)
 	{
-		if (currentNode->rightNode != nullptr) currentNode = currentNode->rightNode; 
-		else break; 
+		while (strcmp(in_key, leaderNode->key) > 0)
+		{
+			numOfComparisons++; 
+
+			if (leaderNode->downNode != nullptr)
+			{
+				lastPathDown = leaderNode;
+			}
+			if (leaderNode->rightNode != nullptr)
+			{
+				leaderNode = leaderNode->rightNode;
+			}
+			else break;
+		}
+		leaderNode = lastPathDown->downNode;
+		searchRow--;
 	}
+
+	//search bottom layer for the new node's place 
+	while (strcmp(in_key, leaderNode->key) > 0)
+	{
+		numOfComparisons++;
+
+		if (leaderNode->rightNode != nullptr)
+		{
+			laggerNode = leaderNode;
+			leaderNode = leaderNode->rightNode;
+		}
+		else if (strcmp(in_key, leaderNode->key) == 0)
+		{
+			_increaseCounters(leaderNode);
+			makeNewNode = false;
+		}
+		else
+		{
+			//reached end of the list 
+			laggerNode = leaderNode;
+			leaderNode = nullptr;
+			break;
+		}
+	}
+
+	//browse express lane to find path down
+	/*
+	while (searchNode->level != 0)
+	{
+		while (strcmp(in_key, searchNode->key) < 0)
+		{
+			numOfComparisons++;
+			if (searchNode->rightNode != nullptr) searchNode = searchNode->rightNode;
+			else searchNode = searchNode->downNode; 
+		}
+	}
+
+	leaderNode = searchNode; 
+	laggerNode = searchNode; 
+	//search bottom layer for the new node's place 
+	while (strcmp(in_key, leaderNode->key) > 0 )
+	{
+		numOfComparisons++; 
+
+		if (leaderNode->rightNode != nullptr)
+		{
+			laggerNode = leaderNode;
+			leaderNode = leaderNode->rightNode;
+		}
+		else if (strcmp(in_key, leaderNode->key) == 0)
+		{
+			_increaseCounters(leaderNode);
+			makeNewNode = false; 
+		}
+		else
+		{
+			//reached end of the list 
+			laggerNode = leaderNode; 
+			leaderNode = nullptr; 
+			break; 
+		}
+	}
+	 */ 
 	//will exit when the in_key is larger than the next node in the list 
 
-
+	if (makeNewNode == true) _createNewNode(laggerNode, leaderNode, in_key); 
 }
 
 void SkipList::_createNewNode(skipNode *leftNode, skipNode *rightNode, char in_key[])
 {
-	skipNode newNode, *newNodePointer, *highestCopy; 
+	skipNode newNode, *newNodePointer;
+	skipNode *highestCopy = nullptr; 
 
 	//if this new node is copied up a row, this will be set to true 
 	bool upperCreated = false; 
 	int level = 0; 
 
 	//newNode should be created in the bottom layer first 
-	newNode.downNode = nullptr; 
+	newNode.downNode = nullptr;
+	newNode.upNode = nullptr; 
 	newNode.leftNode = leftNode; 
 	newNode.rightNode = rightNode; 
-
+	
+	//copy key to node key 
 	strcpy(newNode.key, in_key); 
 
 	//store new node in memory 
 	nodeStorage[nodeStorage_index] = newNode; 
-	newNodePointer = &nodeStorage[nodeStorage_index]; 
+	newNodePointer = &nodeStorage[nodeStorage_index];
+	leftNode->rightNode = &nodeStorage[nodeStorage_index];
+	if (rightNode != nullptr) rightNode->leftNode = &nodeStorage[nodeStorage_index]; 
+
 	nodeStorage_index++; 
 	
-	//copy the key over 
-	strcpy(newNode.key, in_key);
-
+	
 	//the following will create and link copy nodes above the new node 
 	while (_random() == true)
 	{
-		skipNode upNode, *leaderNode, *laggerNode; 
+		std::cout << "Making copy \n"; 
+		skipNode upNode; 
+		skipNode *leaderNode = nullptr; 
+		skipNode *laggerNode = nullptr; 
 		skipNode *nodeSlider = nullptr; 
 
 		level++; 
@@ -80,11 +186,11 @@ void SkipList::_createNewNode(skipNode *leftNode, skipNode *rightNode, char in_k
 		if (level > NumOflevels)
 		{
 			//create the lowest left node 
-			int lowerLevel = NumOflevels - 1; 
+			int lowerLevel = level - 1; 
 			skipNode nextMaxNode; 
 
 			//remove this if the most negative node is changed to null
-			strcmp(nextMaxNode.key, bottomLeftNode->key); 
+			strcpy(nextMaxNode.key, bottomLeftNode->key); 
 
 			//set the level of the new node
 			nextMaxNode.level = level; 
@@ -96,9 +202,11 @@ void SkipList::_createNewNode(skipNode *leftNode, skipNode *rightNode, char in_k
 			
 			nodeStorage[nodeStorage_index] = nextMaxNode; 
 			
+			//died here ----
 			leftNodes[lowerLevel]->upNode = &nodeStorage[nodeStorage_index];
 			nodeStorage[nodeStorage_index].downNode = leftNodes[lowerLevel];
-			leftNodes[NumOflevels] = &nodeStorage[nodeStorage_index]; 
+			leftNodes[level] = &nodeStorage[nodeStorage_index]; 
+			topLeftNode = leftNodes[level]; 
 			nodeStorage_index++; 
 			//increase the number of levels in the class
 			NumOflevels++; 
@@ -106,40 +214,106 @@ void SkipList::_createNewNode(skipNode *leftNode, skipNode *rightNode, char in_k
 
 		leaderNode = leftNodes[level]; 
 		
-		//look for where upper level copy will go in list 
+		while (strcmp(in_key, leaderNode->key) > 0)
+		{
+			numOfComparisons++; 
+
+			if (leaderNode->rightNode != nullptr)
+			{
+				laggerNode = leaderNode;
+				leaderNode = leaderNode->rightNode;
+			}
+			else
+			{
+				//reached end of the list 
+				laggerNode = leaderNode;
+				leaderNode = nullptr;
+				break;
+			}
+		}
+
+		//look for where upper level copy will go in list
+		/*
 		while (strcmp(in_key, leaderNode->key) > 0)
 		{
 			laggerNode = leaderNode; 
 			leaderNode = leaderNode->rightNode; 
 		}
+		*/ 
 		//when this while exits, the laggerNode will contain the left node, and the leftSearchNode will be the right node
 		
+		upNode.level = level; 
 		strcpy(upNode.key, in_key);
-		upNode.leftNode = laggerNode; 
-		upNode.rightNode = leaderNode;
+		if (laggerNode != nullptr) upNode.leftNode = laggerNode; 
+		else upNode.leftNode = leftNodes[level]; 
+
+		if (leaderNode != nullptr) upNode.rightNode = leaderNode;
+		else upNode.rightNode = nullptr; 
+
 		upNode.upNode = nullptr; 
-		
-		/*
-		//go up from the newNode to the level below new copy level to get pointer to downNode
-		nodeSlider = newNodePointer; 
-		for (int j = 0; j < NumOflevels; j++)
-		{
-			nodeSlider = nodeSlider->upNode; 
-		}
-		//upNode.downNode = nodeSlider;
-		*/
 
 		if (highestCopy != nullptr) upNode.downNode = highestCopy;
 		else upNode.downNode = newNodePointer; 
 
-		nodeStorage[nodeStorage_index] = upNode; 
-		highestCopy->upNode = &nodeStorage[nodeStorage_index]; 
+		nodeStorage[nodeStorage_index] = upNode;
+
+		//set pointers of nodes around nextHighest to respect nextHighest 
+		if (highestCopy != nullptr) highestCopy->upNode = &nodeStorage[nodeStorage_index];
+
+		if (nodeStorage[nodeStorage_index].rightNode != nullptr)
+		{
+			nodeStorage[nodeStorage_index].rightNode->leftNode = &nodeStorage[nodeStorage_index];
+		}
+		//left node should never be null pointer, dont need to worry about it 
+		nodeStorage[nodeStorage_index].leftNode->rightNode = &nodeStorage[nodeStorage_index]; 
+
 		highestCopy = &nodeStorage[nodeStorage_index]; 
 		nodeStorage_index++; 
 	}
+
 }
 
 bool SkipList::_random()
 {
+	//should be a 50/50 chance of returning true -- dont think this is what I want, gives the same series each time 
+	bool result = true; 
 
+	if ((rand() % 2) == 1) result = false; 
+
+	return result; 
+}
+
+void SkipList::_traverse()
+{
+	skipNode *currentNode, *farLeftNode; 
+	char empty[50]; 
+	
+	if (topLeftNode != nullptr)
+	{
+		currentNode = topLeftNode; 
+
+		for (int i = NumOflevels; i >= 0; i--)
+		{
+			currentNode = leftNodes[i]; 
+			std::cout << "Level : " << i << "\n"; 
+
+			while (currentNode!= nullptr)
+			{
+				if (currentNode != leftNodes[i]) std::cout << currentNode->key << " , ";
+				currentNode = currentNode->rightNode;
+			} 
+			std::cout << "\n"; 
+		}
+	}
+	else std::cout << "Empty"; 
+}
+
+void SkipList::_increaseCounters(skipNode *targetNode)
+{
+	do
+	{
+		targetNode->counter++; 
+		if (targetNode->upNode != nullptr) targetNode = targetNode->upNode; 
+
+	} while (targetNode->upNode != nullptr);
 }
--- Trees/SkipList.h
+++ Trees/SkipList.h
@@ -6,7 +6,9 @@ public:
 	struct skipNode
 	{
 		char key[50]; 
+		int counter; 
 		skipNode *rightNode, *leftNode, *upNode, *downNode;
+		
 
 		//this will represent what level this node is on 
 		int level; 
@@ -14,23 +16,26 @@ public:
 
 	int NumOflevels = 0;
 
+	//count of number of comparisons made when inserting 
+	int numOfComparisons = 0; 
+
 	skipNode *bottomLeftNode, *topLeftNode; 
 
 	skipNode nodeStorage[10000]; 
 	int nodeStorage_index = 0; 
 
-	//fraction of nodes with level i pointers also having i+1 pointers 
-	float p; 
-
 	skipNode* headerNode;
 	skipNode* leftNodes[300]; 
 
 	void Insert(char in_key[]); 
+	void List(); 
 
 private:
 	void _insert(char in_key[]); 
 	bool _random(); 
 	void _createNewNode(skipNode *leftNode, skipNode *rightNode, char in_key[]); 
+	void _traverse(); 
+	void _increaseCounters(skipNode *targetNode); 
 	
 };
 
